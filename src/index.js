21cd62aa22da9c92249cf41bb8c426f9573cf673
adrilene <adrilenefonseca@gmail.com>
2021-04-15 16:13:25 -0300

    finish section

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import CommentDetail from './CommentDetail'
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import Message from './Message'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Drica"
-        timeAgo="Today at 3:35PM"
-        comment="Good Job"
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Drica"
+          timeAgo="Today at 3:35PM"
+          comment="Good Job"
+          />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this? 
+        </div>
+      </ApprovalCard>
+      
       <CommentDetail 
         author="Diego"
         timeAgo="Today at 11:00AM" 
         comment="I'm hungry"
       />
+      
       <CommentDetail
         author="Dora"
         timeAgo="Yesterday at 10:38PM" 
         comment="I want a hug"
       />
       
+      <Message 
+        header="Changes in Service
+        "
+        text="We just updated our privacy policy here to better service our customers."
+      />
     </div>
+
   );
 };
 
